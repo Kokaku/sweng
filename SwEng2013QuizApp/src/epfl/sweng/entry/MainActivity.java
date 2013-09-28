33e0414a1525300ee936d70c07dc6b384c86e766
Valentin Rutz <valentin.rutz@epfl.ch>
2013-09-28 14:12:40 +0200

    Add ShowQuestionsActivity and EditQuestionActivity. Modify MainActivity to open the right one clicking the right button.

--- SwEng2013QuizApp/src/epfl/sweng/entry/MainActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/entry/MainActivity.java
@@ -1,9 +1,13 @@
 package epfl.sweng.entry;
 
+import showquestions.ShowQuestionsActivity;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import epfl.sweng.R;
+import epfl.sweng.editquestions.EditQuestionActivity;
 import epfl.sweng.testing.TestingTransactions;
 import epfl.sweng.testing.TestingTransactions.TTChecks;
 
@@ -29,6 +33,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void showQuestion(View view) {
+		startActivity(new Intent(this, ShowQuestionsActivity.class));
+	}
+	
+	public void submitQuestion(View view) {
+		startActivity(new Intent(this, EditQuestionActivity.class));
 	}
 
 }
