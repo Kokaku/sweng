d681a4a1396aa8219fce727713faaa326087a18b
Louis Séguy <louis.seguy@epfl.ch>
2013-09-29 16:31:35 +0200

    First draft of ShowQuestion UI.

--- SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
@@ -2,15 +2,34 @@ package epfl.sweng.showquestions;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import epfl.sweng.R;
 
+/**
+ * 
+ * @author lseguy
+ *
+ */
 public class ShowQuestionsActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_show_questions);
+		
+		String[] answers = {"Banana", "Potato", "It's so fluffy!", "Gnaaaah", "Foo", "Bar", "Blah", "Pouet"};
+
+		TextView question = (TextView) findViewById(R.id.text_question);
+		question.setMovementMethod(new ScrollingMovementMethod());
+		
+		ArrayAdapter<String> adapterAnswers = new ArrayAdapter<String>(this,
+		android.R.layout.simple_list_item_1, answers);
+		ListView answersList = (ListView) findViewById(R.id.list_answers);
+		answersList.setAdapter(adapterAnswers);
 	}
 
 	@Override
