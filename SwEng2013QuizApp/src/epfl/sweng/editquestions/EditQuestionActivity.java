92b2e963f0d8f2b22164734e1082c4293fa36edd
Zhivka Gucevska <zhivka.gucevska@epfl.ch>
2013-09-30 01:01:25 +0200

    Initialization of activity_edit_question.

--- SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
@@ -1,6 +1,7 @@
 package epfl.sweng.editquestions;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import epfl.sweng.R;
@@ -11,6 +12,9 @@ public class EditQuestionActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_question);
+		
+		
+		
 	}
 
 	@Override
