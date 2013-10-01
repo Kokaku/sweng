0833ea3d3500a0c470b1e465744d98f6d9b8281a
Louis Séguy <louis.seguy@epfl.ch>
2013-10-01 10:24:33 +0200

    Add tags and improvements to ShowQuestion UI

---
+++ SwEng2013QuizApp/src/epfl/sweng/Question.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package epfl.sweng;
+
+import java.util.Set;
+
+/**
+ * This class represents a question.
+ * 
+ * @author lseguy
+ *
+ */
+
+public class Question {
+	public static final long DEFAULT_ID = 0;
+	
+	private long id;
+	private String question;
+	private String[] answers;
+	private int solutionIndex;
+	private Set<String> tags;
+	
+	/*
+	public Question(String question, String[] answers, int solutionIndex, Set<String> tags) {
+		id = DEFAULT_ID;
+		this.question = question;
+		this.answers = answers;
+		this.solutionIndex = solutionIndex;
+		this.tags = tags;
+	}
+	*/
+	
+	/**
+	 * Default constructor. Create a random question.
+	 */
+	public Question() {
+		
+	}	
+
+	public String getQuestion() {
+		return question;
+	}
+	/*
+	private JSONObject getRandomFromServer() {
+		HttpGet randomGet = new HttpGet("https://sweng-quiz.appspot.com/quizquestions/random");
+		ResponseHandler<String> randomHandler = new BasicResponseHandler();
+		try {
+			String question = SwengHttpClientFactory.getInstance().execute(randomGet, randomHandler);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	*/
+}
--- SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import epfl.sweng.R;
@@ -21,13 +23,24 @@ public class ShowQuestionsActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_show_questions);
 		
+		String[] tags = {"Despicable Me", "Unicorn", "Fluffiness", "Some tag", "Rainbow", "Blah"};
+		
+		ArrayAdapter<String> adapterTags = new ArrayAdapter<String>(this,
+				R.layout.list_of_tags, tags);
+		
+		LinearLayout tagsLayout = (LinearLayout) findViewById(R.id.list_tags);
+		for (int i = 0; i < adapterTags.getCount(); i++) {
+			View item = adapterTags.getView(i, null, null);
+			tagsLayout.addView(item);
+		}
+		
 		String[] answers = {"Banana", "Potato", "It's so fluffy!", "Gnaaaah", "Foo", "Bar", "Blah", "Pouet"};
 
 		TextView question = (TextView) findViewById(R.id.text_question);
 		question.setMovementMethod(new ScrollingMovementMethod());
 		
 		ArrayAdapter<String> adapterAnswers = new ArrayAdapter<String>(this,
-		android.R.layout.simple_list_item_1, answers);
+				android.R.layout.simple_list_item_1, answers);
 		ListView answersList = (ListView) findViewById(R.id.list_answers);
 		answersList.setAdapter(adapterAnswers);
 	}
