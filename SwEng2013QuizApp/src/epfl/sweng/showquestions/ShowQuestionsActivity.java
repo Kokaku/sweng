73451f50f4a07ed16ad360806769ba26849f6013
Louis Séguy <louis.seguy@epfl.ch>
2013-10-01 21:17:34 +0200

    Clean up ShowQuestionsActivity

--- SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
@@ -3,17 +3,16 @@ package epfl.sweng.showquestions;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
-import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
 import android.widget.ListView;
 import android.widget.TextView;
 import epfl.sweng.R;
 
 /**
+ * This activity displays questions and allows the user to answer them.
+ * The user has to chose the right answer to be able to receive a new question.
  * 
  * @author lseguy
  * 
@@ -25,35 +24,39 @@ public class ShowQuestionsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_questions);
 
-        String[] tags = { "Despicable Me", "Unicorn", "Fluffiness", "Some tag",
-                "Rainbow", "Blah" };
+        
+        /*
+         * Initialize tags list with static data.
+         */
+        String[] tags = {"Despicable Me", "Unicorn", "Fluffiness", "Some tag",
+            "Rainbow", "Blah"};
 
+        // Using an adapter to fill the LinearLayout with data from the array
         ArrayAdapter<String> adapterTags = new ArrayAdapter<String>(this,
-                R.layout.list_of_tags, tags);
+            R.layout.list_of_tags, tags);
 
         LinearLayout tagsLayout = (LinearLayout) findViewById(R.id.list_tags);
-        for (int i = 0; i < adapterTags.getCount(); i++) {
+        
+        for (int i = 0; i < adapterTags.getCount(); ++i) {
             View item = adapterTags.getView(i, null, tagsLayout);
             tagsLayout.addView(item);
         }
 
-        String[] answers = { "Banana", "Potato", "It's so fluffy!", "Gnaaaah",
-                "Foo", "Bar", "Blah", "Pouet" };
+        
+        /*
+         * Initialize answers list with static data.
+         */
+        String[] answers = {"Banana", "Potato", "It's so fluffy!", "Gnaaaah",
+            "Foo", "Bar", "Blah", "Pouet"};
 
         TextView question = (TextView) findViewById(R.id.text_question);
         question.setMovementMethod(new ScrollingMovementMethod());
 
         ArrayAdapter<String> adapterAnswers = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, answers);
+            android.R.layout.simple_list_item_1, answers);
+        
         ListView answersList = (ListView) findViewById(R.id.list_answers);
         answersList.setAdapter(adapterAnswers);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.show_questions, menu);
-        return true;
     }
 
 }
