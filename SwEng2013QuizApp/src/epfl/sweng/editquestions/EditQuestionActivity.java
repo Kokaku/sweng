41151743cd9686e5749ec9e21ce949489c8f453c
MathieuMonney <m.monney@epfl.ch>
2013-10-02 12:42:12 +0200

    Begin the implementation of EditQuestionActivity, not working yet. Shouldn't prevent the code from compiling.

--- SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
@@ -1,44 +1,53 @@
 package epfl.sweng.editquestions;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import epfl.sweng.R;
 
 public class EditQuestionActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_question);
 
-        String[] tags = {};
-        String answers[] = {};
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_question);
+		
+		String[] tags = {};
+		String[] answers = {};
+	}
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.edit_question, menu);
-        return true;
-    }
-
-    // public boolean onClickSubmit() {
-    // ListView listView = (ListView) findViewById(R.id.list_new_answers);
-    // ListAdapter listAdapter = listView.getAdapter();
-    // List<String> answers;
-    // int answersNumber = listAdapter.getCount();
-    // for(int i = 0; i<answersNumber; ++i){
-    // answers.add(listAdapter.getView(i, null, listView));
-    //
-    // }
-    //
-    // }
-
-    // public boolean onClickAdd() {
-    //
-    // return true;
-    // }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.edit_question, menu);
+		return true;
+	}
+	
+	public boolean onClickSubmit() {
+		ListView listView = (ListView) findViewById(R.id.list_answers);
+		ListAdapter listAdapter = listView.getAdapter();
+		String answerString = listAdapter.toString(); // To test
+		ArrayList<String> answers = new ArrayList<String>();
+		int answersNumber = listAdapter.getCount();
+		for (int i = 0; i<answersNumber; ++i) {
+			answers.add(((TextView) listAdapter.getView(i, null, listView)).getText().toString());
+		}
+		return true;
+	}
+	
+	public boolean onClickAdd() {
+	    ListView listView = (ListView) findViewById(R.id.list_answers);
+	    LinearLayout newLine = new LinearLayout(listView.getContext());
+	    //listView.addView(child, listView.getCount(), params);
+		return true;
+	}
+>>>>>>> Begin the implementation of EditQuestionActivity, not working yet. Shouldn't prevent the code from compiling.
 
 }
