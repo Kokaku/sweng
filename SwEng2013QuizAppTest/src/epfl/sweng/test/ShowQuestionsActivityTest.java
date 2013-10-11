0eb34df8b42306fc432dc81c4b5ead708a84ae1c
Louis Séguy <louis.seguy@epfl.ch>
2013-10-11 11:47:55 +0200

    Add first test

---
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
@@ -0,0 +1,21 @@
+package epfl.sweng.test;
+
+import epfl.sweng.showquestions.ShowQuestionsActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * @author lseguy
+ *
+ */
+public class ShowQuestionsActivityTest extends
+    ActivityInstrumentationTestCase2<ShowQuestionsActivity> {
+    
+    public ShowQuestionsActivityTest() {
+        super(ShowQuestionsActivity.class);
+    }
+
+    public void testHello() {
+        fail("Not yet implemented");
+    }
+    
+}
