762e73c8d02017c0a6e26e077c61f43447266133
Louis Séguy <louis.seguy@epfl.ch>
2013-10-11 22:41:40 +0200

    Revert "Remove test project to investigate Jenkins build failure"
    This reverts commit 0ac7f528aade7cfd8550511f365bb14134b68885.
    It didn't solve the issue.

---
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
@@ -0,0 +1,17 @@
+package epfl.sweng.test;
+
+import epfl.sweng.showquestions.ShowQuestionsActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * @author lseguy
+ *
+ */
+public class ShowQuestionsActivityTest extends
+    ActivityInstrumentationTestCase2<ShowQuestionsActivity> {
+    
+    public ShowQuestionsActivityTest() {
+        super(ShowQuestionsActivity.class);
+    }
+    
+}
