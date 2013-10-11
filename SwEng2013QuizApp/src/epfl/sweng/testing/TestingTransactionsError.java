6bb1345eeff17af2f516e0835f21d0751ac611d6
Louis Séguy <louis.seguy@epfl.ch>
2013-10-11 16:16:05 +0200

    Update testing transactions files

--- SwEng2013QuizApp/src/epfl/sweng/testing/TestingTransactionsError.java
+++ SwEng2013QuizApp/src/epfl/sweng/testing/TestingTransactionsError.java
@@ -1,11 +1,13 @@
 package epfl.sweng.testing;
 
-/// An exception that is thrown whenever TestingTransactions are used in a wrong
-/// way.
+/**
+ * An exception that is thrown whenever TestingTransactions are used in a wrong
+ * way.
+ */
 public class TestingTransactionsError extends AssertionError {
-    public TestingTransactionsError(String msg) {
-        super(msg);
-    }
+	public TestingTransactionsError(String msg) {
+		super(msg);
+	}
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 }
