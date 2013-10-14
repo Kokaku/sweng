df31e057db64ceeb7096063b9f83df190144c340
MathieuMonney <m.monney@epfl.ch>
2013-10-14 15:27:17 +0200

    Adds testing class EditQuestionActivityTest.

---
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/EditQuestionActivityTest.java
@@ -0,0 +1,17 @@
+package epfl.sweng.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import epfl.sweng.editquestions.EditQuestionActivity;
+
+/**
+ * @author MathieuMonney
+ * 
+ */
+public class EditQuestionActivityTest extends
+        ActivityInstrumentationTestCase2<EditQuestionActivity> {
+
+    public EditQuestionActivityTest() {
+        super(EditQuestionActivity.class);
+    }
+
+}
