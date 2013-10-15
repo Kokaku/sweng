de96a76ed70b46589df56f4c5512c6dc50edea71
Louis Séguy <louis.seguy@epfl.ch>
2013-10-15 19:11:42 +0200

    Create testing framework

--- SwEng2013QuizAppTest/src/epfl/sweng/test/QuizAppTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/QuizAppTest.java
@@ -5,22 +5,54 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import com.jayway.android.robotium.solo.Solo;
 
+import epfl.sweng.testing.TestCoordinator;
+import epfl.sweng.testing.TestingTransaction;
+
 /**
- * @author MathieuMonney
- * 
+ * @author lseguy
+ *
  */
-public abstract class QuizAppTest<T extends Activity> extends
-        ActivityInstrumentationTestCase2<T> {
-
+public abstract class QuizAppTest<T extends Activity>
+    extends ActivityInstrumentationTestCase2<T>
+    {
+    
     protected Solo solo;
-
+    
     public QuizAppTest(Class<T> activityClass) {
         super(activityClass);
     }
-
+    
     @Override
-    protected void setUp() {
-        solo = new Solo(getInstrumentation(), getActivity());
+    protected void setUp() throws Exception {
+        super.setUp();
+        solo = new Solo(getInstrumentation());
     }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+        super.tearDown();
+    }
+    
+    protected void getActivityAndWaitFor(final TestCoordinator.TTChecks expected) {
+        TestCoordinator.run(getInstrumentation(), new TestingTransaction() {
+          @Override
+          public void initiate() {
+            getActivity();
+          }
 
+          @Override
+          public void verify(TestCoordinator.TTChecks notification) {
+            assertEquals(String.format(
+                "Expected notification %s, but received %s", expected,
+                notification), expected, notification);
+          }
+
+          @Override
+          public String toString() {
+            return String.format("getActivityAndWaitFor(%s)", expected);
+          }
+        });
+      }
+    
 }
