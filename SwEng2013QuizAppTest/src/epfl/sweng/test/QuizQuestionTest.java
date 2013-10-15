50dda66944a1d8f84a620460a85a3a0eb138e749
Louis Séguy <louis.seguy@epfl.ch>
2013-10-15 21:53:30 +0200

    Add tests for QuizQuestion

--- SwEng2013QuizAppTest/src/epfl/sweng/test/QuizQuestionTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/QuizQuestionTest.java
@@ -3,18 +3,84 @@
  */
 package epfl.sweng.test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import android.test.AndroidTestCase;
+import epfl.sweng.questions.QuizQuestion;
 
 /**
  * @author lseguy
  *
  */
 public class QuizQuestionTest extends AndroidTestCase {
+    
+    private QuizQuestion question;
+    private Set<String> tags;
+    
+    public static final int CORRECT_ANSWER_ID = 1;
+    public static final String QUESTION_TEXT = "This is the question";
+    public static final String[] LIST_OF_ANSWERS = {"Answer 1", "Answer 2"};
 
     protected void setUp() throws Exception {
         super.setUp();
+        
+        tags = new HashSet<String>();
+        tags.add("Tag1");
+        tags.add("Tag2");
+        question = new QuizQuestion(QUESTION_TEXT,
+            LIST_OF_ANSWERS, CORRECT_ANSWER_ID, tags);
+    }
+
+    public void testCreateQuestionWithNull() {
+        try {
+            @SuppressWarnings("unused")
+            QuizQuestion illegalQuestion = new QuizQuestion(null, null, 0,
+                null);
+            fail("Constructor can't accept null arguments");
+        }
+        catch (IllegalArgumentException e) {
+            
+        }
     }
     
+    public void testCheckCorrectAnswerIsTrue() {
+        assertTrue(question.isSolutionCorrect(CORRECT_ANSWER_ID));
+    }
     
+    public void testCheckCorrectAnswerIsFalse() {
+        for (int i = 0; i < LIST_OF_ANSWERS.length; ++i) {
+            if (i != CORRECT_ANSWER_ID) {
+                assertFalse(question.isSolutionCorrect(i));
+            }
+        }
+    }
+    
+    public void testGetQuestionText() {
+        assertEquals(QUESTION_TEXT, question.getQuestion());
+    }
+    
+    public void testGetSolutionIndex() {
+        assertEquals(CORRECT_ANSWER_ID, question.getSolutionIndex());
+    }
+    
+    public void testGetAnswersCopy() {
+        assertNotSame("getAnswers getter must return a copy of the array",
+            LIST_OF_ANSWERS, question.getAnswers());
+    }
+    
+    public void testGetAnswers() {
+        assertTrue(Arrays.equals(LIST_OF_ANSWERS, question.getAnswers()));
+    }
+    
+    public void testGetTagsCopy() {
+        assertNotSame("getTags getter must return a copy of the set",
+            tags, question.getTags());
+    }
+    
+    public void testGetTags() {
+        assertEquals(tags, question.getTags());
+    }
 
 }
--- SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
@@ -1,8 +1,13 @@
 package epfl.sweng.test;
 
+import org.apache.http.HttpStatus;
+
 import android.widget.Button;
+import epfl.sweng.servercomm.SwengHttpClientFactory;
 import epfl.sweng.showquestions.ShowQuestionsActivity;
 import epfl.sweng.test.framework.QuizActivityTestCase;
+import epfl.sweng.test.minimalmock.MockHttpClient;
+import epfl.sweng.testing.TestCoordinator.TTChecks;
 
 /**
  * @author lseguy
@@ -12,18 +17,34 @@ import epfl.sweng.test.framework.QuizActivityTestCase;
 public class ShowQuestionsActivityTest extends
         QuizActivityTestCase<ShowQuestionsActivity> {
 
+    private MockHttpClient mockHttpClient;
+
     public ShowQuestionsActivityTest() {
         super(ShowQuestionsActivity.class);
     }
 
+    protected void setUp() throws Exception {
+        super.setUp();
+        mockHttpClient = new MockHttpClient();
+        SwengHttpClientFactory.setInstance(mockHttpClient);
+    }
+    
     public void testShowQuestion() {
+        mockHttpClient
+                .pushCannedResponse(
+                        "GET (?:https?://[^/]+|[^/]+)?/+quizquestions/random\\b",
+                        HttpStatus.SC_OK,
+                        "{\"question\": \"What is the answer to life, the universe, and everything?\","
+                                + " \"answers\": [\"Forty-two\", \"Twenty-seven\"], \"owner\": \"sweng\","
+                                + " \"solutionIndex\": 0, \"tags\": [\"h2g2\", \"trivia\"], \"id\": \"1\" }",
+                        "application/json");
+        getActivityAndWaitFor(TTChecks.QUESTION_SHOWN);
         assertTrue(
                 "Question is displayed",
-                solo.searchText("What is the answer to Life, the universe and everything?"));
+                solo.searchText("What is the answer to life, the universe, and everything?"));
         assertTrue("Correct answer is displayed", solo.searchText("Forty-two"));
         assertTrue("Incorrect answer is displayed",
                 solo.searchText("Twenty-seven"));
-
         Button nextQuestionButton = solo.getButton("Next question");
         assertFalse("Next question button is disabled",
                 nextQuestionButton.isEnabled());
