230a262d3a0913b1651b4e0f3c1d05ae97bfe1d2
Zhivka Gucevska <zhivka.gucevska@epfl.ch>
2013-10-16 13:20:31 +0200

    Adds some test in EditQuestionActivityTest. git add . When tests for this activity launched, AssertionError in ShowQuestionActivityTest occurs git add .

--- SwEng2013QuizAppTest/src/epfl/sweng/test/EditQuestionActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/EditQuestionActivityTest.java
@@ -1,17 +1,48 @@
 package epfl.sweng.test;
 
 import epfl.sweng.editquestions.EditQuestionActivity;
+import epfl.sweng.servercomm.SwengHttpClientFactory;
 import epfl.sweng.test.framework.QuizActivityTestCase;
+import epfl.sweng.test.minimalmock.MockHttpClient;
+import epfl.sweng.testing.TestCoordinator.TTChecks;
 
 /**
  * @author MathieuMonney
  * 
  */
-public class EditQuestionActivityTest extends QuizActivityTestCase<EditQuestionActivity> {
+public class EditQuestionActivityTest extends
+		QuizActivityTestCase<EditQuestionActivity> {
 
-    public EditQuestionActivityTest() {
-        super(EditQuestionActivity.class);
-    }
+	private MockHttpClient mockHttpClient;
 
+	public EditQuestionActivityTest() {
+		super(EditQuestionActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		mockHttpClient = new MockHttpClient();
+		SwengHttpClientFactory.setInstance(mockHttpClient);
+		getActivityAndWaitFor(TTChecks.EDIT_QUESTIONS_SHOWN);
+	}
+
+	public void testQuestionFieldDisplayed() {
+		assertTrue("Question field is displayed",
+				solo.searchEditText("Type in the question's text body"));
+	}
+
+	public void testTagsFieldDisplayed() {
+		assertTrue("Tags field is displayed",
+				solo.searchEditText("Type in the question's tags"));
+	}
+
+	public void testAnswerFieldIsDisplayed() {
+		assertTrue("Answer field is displayed",
+				solo.searchEditText("Type in the answer"));
+	}
+
+	public void testAddAnswerButton() {
+		assertTrue("Button for adding answers is displayed ",
+				solo.searchButton("\u002B"));
+	}
 }
-
