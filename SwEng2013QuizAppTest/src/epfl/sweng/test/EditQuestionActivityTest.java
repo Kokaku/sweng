5777c506fdd31088cb4e5c2ef3c1020f51da4627
Zhivka Gucevska <zhivka.gucevska@epfl.ch>
2013-10-16 18:05:50 +0200

    Adds tests to EditQuestionActivityTest

--- SwEng2013QuizAppTest/src/epfl/sweng/test/EditQuestionActivityTest.java
+++ SwEng2013QuizAppTest/src/epfl/sweng/test/EditQuestionActivityTest.java
@@ -14,63 +14,85 @@ import epfl.sweng.testing.TestCoordinator.TTChecks;
  * 
  */
 public class EditQuestionActivityTest extends
-        QuizActivityTestCase<EditQuestionActivity> {
+		QuizActivityTestCase<EditQuestionActivity> {
 
-    private MockHttpClient mockHttpClient;
+	private MockHttpClient mockHttpClient;
 
-    public EditQuestionActivityTest() {
-        super(EditQuestionActivity.class);
-    }
+	public EditQuestionActivityTest() {
+		super(EditQuestionActivity.class);
+	}
 
-    protected void setUp() throws Exception {
-        super.setUp();
-        mockHttpClient = new MockHttpClient();
-        SwengHttpClientFactory.setInstance(mockHttpClient);
+	protected void setUp() throws Exception {
+		super.setUp();
+		mockHttpClient = new MockHttpClient();
+		SwengHttpClientFactory.setInstance(mockHttpClient);
 
-        mockHttpClient
-                .pushCannedResponse(
-                        "GET (?:https?://[^/]+|[^/]+)?/+quizquestions/random\\b",
-                        HttpStatus.SC_OK,
-                        "{\"question\": \"What is the answer to life, the universe, and everything?\","
-                                + " \"answers\": [\"Forty-two\", \"Twenty-seven\"], \"owner\": \"sweng\","
-                                + " \"solutionIndex\": 0, \"tags\": [\"h2g2\", \"trivia\"], \"id\": \"1\" }",
-                        "application/json");
+		mockHttpClient
+				.pushCannedResponse(
+						"GET (?:https?://[^/]+|[^/]+)?/+quizquestions/random\\b",
+						HttpStatus.SC_OK,
+						"{\"question\": \"What is the answer to life, the universe, and everything?\","
+								+ " \"answers\": [\"Forty-two\", \"Twenty-seven\"], \"owner\": \"sweng\","
+								+ " \"solutionIndex\": 0, \"tags\": [\"h2g2\", \"trivia\"], \"id\": \"1\" }",
+						"application/json");
 
-        getActivityAndWaitFor(TTChecks.EDIT_QUESTIONS_SHOWN);
-    }
+		getActivityAndWaitFor(TTChecks.EDIT_QUESTIONS_SHOWN);
+	}
 
-    public void testQuestionFieldDisplayed() {
-        assertTrue("Question field is displayed",
-                solo.searchEditText("Type in the question's text body"));
-    }
+	private void addAnswer(){
+		
+	}
+	
+	private void removeAnswer(){
+		
+	}
+	
+	private void checkAsCorrect(){
+		
+	}
+	private void submitQuestion(){
+		
+	}
+	
+	
+	public void testQuestionFieldDisplayed() {
+		assertTrue("Question field is displayed",
+				solo.searchEditText("Type in the question's text body"));
+	}
 
-    public void testTagsFieldDisplayed() {
-        assertTrue("Tags field is displayed",
-                solo.searchEditText("Type in the question's tags"));
-    }
+	public void testTagsFieldDisplayed() {
+		assertTrue("Tags field is displayed",
+				solo.searchEditText("Type in the question's tags"));
+	}
 
-    public void testAnswerFieldIsDisplayed() {
-        assertTrue("Answer field is displayed",
-                solo.searchEditText("Type in the answer"));
-    }
+	public void testAnswerFieldIsDisplayed() {
+		assertTrue("Answer field is displayed",
+				solo.searchEditText("Type in the answer"));
+	}
 
-    public void testAddAnswerButtonDisplayed() {
-        assertTrue("Button for adding answers is displayed ",
-                solo.searchButton("\u002B"));
-    }
+	public void testAddAnswerButtonDisplayed() {
+		assertTrue("Button for adding answers is displayed ",
+				solo.searchButton("\u002B"));
+	}
 
-    public void testRemoveAnswerButtonDisplayed() {
-        assertTrue("Button for removing answers is displayed ",
-                solo.searchButton("\u002D"));
-    }
+	public void testRemoveAnswerButtonDisplayed() {
+		assertTrue("Button for removing answers is displayed ",
+				solo.searchButton("\u002D"));
+	}
 
-    public void testSubmitButtonDisplayed() {
-        assertTrue("Submit button is displayed", solo.searchButton("Submit"));
-    }
+	public void testCheckAnswerButtonDisplayed() {
+		assertTrue("Button for checking an answer is desplayed as wrong",
+				solo.searchButton("\u2714"));
+	}
 
-    public void testSubmitButtonIsInitiallyDisabled() {
-        Button submitButton = solo.getButton("Submit");
-        assertFalse("Submit button is initially disabled",
-                submitButton.isEnabled());
-    }
+	public void testSubmitButtonDisplayed() {
+		assertTrue("Submit button is displayed", solo.searchButton("Submit"));
+	}
+
+	public void testSubmitButtonIsInitiallyDisabled() {
+		Button submitButton = solo.getButton("Submit");
+		assertFalse("Submit button is initially disabled",
+				submitButton.isEnabled());
+	}
+	
 }
