ca8ae4eb5e67c87818403eaf6e1e50bce9218432
MathieuMonney <m.monney@epfl.ch>
2013-10-18 17:26:27 +0200

    Updates main activity to allow Tequila authentication.

---
+++ SwEng2013QuizApp/src/epfl/sweng/authentication/AuthenticationState.java
@@ -0,0 +1,21 @@
+package epfl.sweng.authentication;
+
+/**
+ *
+ * @author MathieuMonney
+ *
+ */
+public enum AuthenticationState {
+    UNAUTHENTICATED, TOKEN, TEQUILA, CONFIRMATION, AUTHENTICATED;
+    
+    
+    private static AuthenticationState current;
+    
+    public static void setState(AuthenticationState newState) {
+        current = newState;
+    }
+    
+    public static AuthenticationState getState() {
+        return current;
+    }
+}
--- SwEng2013QuizApp/src/epfl/sweng/entry/MainActivity.java
+++ SwEng2013QuizApp/src/epfl/sweng/entry/MainActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import epfl.sweng.R;
+import epfl.sweng.authentication.AuthenticationActivity;
+import epfl.sweng.authentication.AuthenticationState;
 import epfl.sweng.editquestions.EditQuestionActivity;
 import epfl.sweng.showquestions.ShowQuestionsActivity;
 import epfl.sweng.testing.TestCoordinator;
@@ -19,17 +22,38 @@ import epfl.sweng.testing.TestCoordinator.TTChecks;
  */
 
 public class MainActivity extends Activity {
+    private Button mShowQuestions;
+    private Button mEditQuestion;
+    private Button mTequilaLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AuthenticationState.setState(AuthenticationState.UNAUTHENTICATED);
         TestCoordinator.check(TTChecks.MAIN_ACTIVITY_SHOWN);
+        mShowQuestions = (Button) findViewById(R.id.show_random_question_button);
+        mEditQuestion = (Button) findViewById(R.id.edit_question_button);
+        mTequilaLogin = (Button) findViewById(R.id.tequila_login_button);
+    }
+
+    protected void onResume() {
+        if (AuthenticationState.getState() != AuthenticationState.AUTHENTICATED) {
+            mShowQuestions.setEnabled(false);
+            mEditQuestion.setEnabled(false);
+            mTequilaLogin.setEnabled(true);
+        } else {
+            mShowQuestions.setEnabled(true);
+            mEditQuestion.setEnabled(true);
+            mTequilaLogin.setEnabled(false);
+        }
     }
 
     /**
      * When the first button is pressed, starts ShowQuestionsActivity
-     * @param view the button which is pressed
+     * 
+     * @param view
+     *            the button which is pressed
      */
     public void showQuestion(View view) {
         startActivity(new Intent(this, ShowQuestionsActivity.class));
@@ -37,10 +61,24 @@ public class MainActivity extends Activity {
 
     /**
      * When the second button is pressed, starts EditQuestionActivity
-     * @param view the button which is pressed
+     * 
+     * @param view
+     *            the button which is pressed
      */
     public void submitQuestion(View view) {
         startActivity(new Intent(this, EditQuestionActivity.class));
+    }
+
+    /**
+     * When the second button is pressed, starts AuthenticationActivity
+     * 
+     * @param view
+     *            the button which is pressed
+     */
+    public void tequilaLogin(View view) {
+        if (AuthenticationState.getState() != AuthenticationState.AUTHENTICATED) {
+            startActivity(new Intent(this, AuthenticationActivity.class));
+        }
     }
 
 }
