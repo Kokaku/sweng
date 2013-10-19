446aa7b9050e888892a13738b9b42d809f82f20e
Junker Florian <florian.junker@epfl.ch>
2013-10-20 01:14:07 +0200

    finish login method in ServerComm And factored it a bit. (I still have some issus about reconnizing error or bad request for authTequila)

--- SwEng2013QuizApp/src/epfl/sweng/servercomm/ServerCommunication.java
+++ SwEng2013QuizApp/src/epfl/sweng/servercomm/ServerCommunication.java
@@ -15,6 +15,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import epfl.sweng.authentication.AuthenticationState;
+import epfl.sweng.authentication.UserCredentials;
 import epfl.sweng.questions.QuizQuestion;
 import epfl.sweng.utils.JSONUtilities;
 
@@ -42,16 +43,21 @@ public final class ServerCommunication {
      * @return true if the question has been correctly sent
      */
     public static boolean send(QuizQuestion question) {
+        if (AuthenticationState.getState() != AuthenticationState.AUTHENTICATED) {
+            return false;
+        }
+        
         if (question != null) {
             try {
                 HttpPost request = new HttpPost(SERVER_URL);
-                request.setEntity(new StringEntity(
-                        JSONUtilities.getJSONString(question)));
+                request.setEntity(new StringEntity(JSONUtilities
+                        .getJSONString(question)));
                 request.setHeader("Content-type", "application/json");
-                
+                // TODO waiting for UserCredentials completion to get sessionID
+                request.setHeader("Authorization", "Tequila " + "sessionID");
+
                 String httpAnswer = new HttpTask().execute(request).get();
-                return httpAnswer != null &&
-                        !httpAnswer.equals("error");
+                return httpAnswer != null && !httpAnswer.equals("error");
             } catch (InterruptedException e) {
             } catch (ExecutionException e) {
             } catch (JSONException e) {
@@ -68,11 +74,17 @@ public final class ServerCommunication {
      * @return the random question fetched, null if an error occurred
      */
     public static QuizQuestion getRandomQuestion() {
+        if (AuthenticationState.getState() != AuthenticationState.AUTHENTICATED) {
+            return null;
+        }
+        
         try {
             HttpUriRequest request = new HttpGet(SERVER_URL + "random");
-            
+            // TODO waiting for UserCredentials completion to get sessionID
+            request.setHeader("Authorization", "Tequila " + "sessionID");
+
             String httpAnswer = new HttpTask().execute(request).get();
-            if (httpAnswer != null  && !httpAnswer.equals("error")) {
+            if (httpAnswer != null && !httpAnswer.equals("error")) {
                 JSONObject json = new JSONObject(httpAnswer);
                 return new QuizQuestion(json.getString("question"),
                         JSONUtilities.parseAnswers(json),
@@ -89,36 +101,41 @@ public final class ServerCommunication {
     }
 
     /**
-     * @return
+     * This method is called to authenticate user and get a session id
+     * You must be in state "AuthenticationState.UNAUTHENTICATED" before calling
+     * this method.
+     * If this method return false you can be in any state of authentication
+     * 
+     * @param username a String representing the user to log
+     * @param password aString representing the password of the user
+     * @return true if correctly authenticated
      */
     public static boolean login(String username, String password) {
+        if (AuthenticationState.getState() != AuthenticationState.UNAUTHENTICATED) {
+            return false;
+        }
+
         try {
-            HttpUriRequest request = new HttpGet(SERVER_LOGIN_URL);
-            String httpAnswer = new HttpTask().execute(request).get();
+            String httpAnswer = requestToken();
+            AuthenticationState.setState(AuthenticationState.TOKEN);
             if (httpAnswer != null) {
                 JSONObject json = new JSONObject(httpAnswer);
                 String token = json.getString("token");
-                AuthenticationState.setState(AuthenticationState.TOKEN);
-
-                ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
-                params.add(new BasicNameValuePair("requestkey", token));
-                params.add(new BasicNameValuePair("username", username));
-                params.add(new BasicNameValuePair("password", password));
-
-                HttpPost postRequest = new HttpPost(TEQUILA_URL);
-                postRequest.setEntity(new UrlEncodedFormEntity(params));
-                postRequest.setHeader("Content-type", "application/json");
                 
-                httpAnswer = new HttpTask().execute(postRequest).get();
-                System.out.println("BANANA1 "+ httpAnswer);
-                if (httpAnswer == null) {
-                    AuthenticationState.setState(AuthenticationState.TEQUILA);
-                    
-                    postRequest = new HttpPost(SERVER_LOGIN_URL);
-                    postRequest.setEntity(new StringEntity("\"token\": \""+token+"\""));
-                    postRequest.setHeader("Content-type", "application/json");
-                    
-                    System.out.println("BANANA2 "+ httpAnswer);
+                //Don't know how to control if the request is a success or a fail
+                httpAnswer = authTequila(token, username, password);
+                AuthenticationState.setState(AuthenticationState.TEQUILA);
+
+                httpAnswer = requestSessionID(token);
+                AuthenticationState.setState(AuthenticationState.CONFIRMATION);
+                
+                if (httpAnswer != null) {
+                    json = new JSONObject(httpAnswer);
+                    String session = json.getString("session");
+                    AuthenticationState
+                            .setState(AuthenticationState.AUTHENTICATED);
+                    UserCredentials.INSTANCE.saveUserCredentials(session);
+                    return true;
                 }
             }
         } catch (InterruptedException e) {
@@ -130,4 +147,42 @@ public final class ServerCommunication {
         return false;
     }
 
+    private static String requestToken() throws
+        InterruptedException, ExecutionException {
+        
+        HttpUriRequest request = new HttpGet(SERVER_LOGIN_URL);
+        // Header is HTTP/1.1 200 ok
+        return new HttpTask().execute(request).get();
+    }
+
+    private static String authTequila(
+            String token,
+            String username,
+            String password) throws InterruptedException,
+                                    ExecutionException,
+                                    UnsupportedEncodingException {
+        
+        ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("requestkey", token));
+        params.add(new BasicNameValuePair("username", username));
+        params.add(new BasicNameValuePair("password", password));
+
+        HttpPost postRequest = new HttpPost(TEQUILA_URL);
+        postRequest.setEntity(new UrlEncodedFormEntity(params));
+        postRequest.setHeader("Content-type", "application/json");
+        // Some errors possible ??
+        // Normal connection return string "error" due to a ioException
+        // But header is HTTP/1.1 302 Found but don't know how to read it
+        return new HttpTask().execute(postRequest).get();
+    }
+
+    private static String requestSessionID(String token) throws
+        UnsupportedEncodingException, InterruptedException, ExecutionException {
+        
+        HttpPost postRequest = new HttpPost(SERVER_LOGIN_URL);
+        postRequest.setEntity(new StringEntity("{\"token\": \"" + token
+                + "\"}"));
+        postRequest.setHeader("Content-type", "application/json");
+        return new HttpTask().execute(postRequest).get();
+    }
 }
