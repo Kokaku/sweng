29e1d155a7670d664d571b6fbe9d97303bf0a590
Louis Séguy <louis.seguy@epfl.ch>
2013-10-29 17:14:29 +0100

    Add javadoc to the SwEng2013QuizApp class

--- SwEng2013QuizApp/src/epfl/sweng/SwEng2013QuizApp.java
+++ SwEng2013QuizApp/src/epfl/sweng/SwEng2013QuizApp.java
@@ -1,12 +1,12 @@
-/**
- * 
- */
 package epfl.sweng;
 
 import android.app.Application;
 import android.content.Context;
 
 /**
+ * This class stores the application context to make it available to other
+ * classes in a static fashion.
+ * 
  * @author lseguy
  *
  */
@@ -14,12 +14,19 @@ public class SwEng2013QuizApp extends Application {
 
     private static Context context;
 
+    /**
+     * Initialization
+     */
     public void onCreate() {
         super.onCreate();
         SwEng2013QuizApp.context = getApplicationContext();
     }
 
+    /**
+     * @return the application context
+     */
     public static Context getAppContext() {
         return SwEng2013QuizApp.context;
     }
+    
 }
