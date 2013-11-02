a84f309c28dc80a7768660ca4a8ff2365758670d
Louis Séguy <louis.seguy@epfl.ch>
2013-11-02 14:10:24 +0100

    Improve hierarchy of exceptions

--- SwEng2013QuizApp/src/epfl/sweng/exceptions/InvalidCredentialsException.java
+++ SwEng2013QuizApp/src/epfl/sweng/exceptions/InvalidCredentialsException.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package epfl.sweng.exceptions;
 
 /**
--- SwEng2013QuizApp/src/epfl/sweng/exceptions/ServerCommunicationException.java
+++ SwEng2013QuizApp/src/epfl/sweng/exceptions/ServerCommunicationException.java
@@ -1,18 +1,12 @@
-/**
- * 
- */
 package epfl.sweng.exceptions;
 
 /**
  * Should be thrown when a network request is unsuccessful.
  * 
- * For instance can be thrown instead of an ExecutionException thrown by an
- * {@link AsyncTask}
- * 
  * @author lseguy
  *
  */
-public class ServerCommunicationException extends Exception {
+public class ServerCommunicationException extends CommunicationException {
  
     private static final long serialVersionUID = 1171644294660709870L;
 
